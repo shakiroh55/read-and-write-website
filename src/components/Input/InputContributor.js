296d1e9a7c1ffd4023ae05698b5b63a1a1162c04
shakiroh55 <fhatshakirat@gmail.com>
2023-10-08 19:03:09 +0100

    changes made

--- src/components/Input/InputContributor.js
+++ src/components/Input/InputContributor.js
@@ -1,28 +1,54 @@
 import React from 'react'
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
 import Styles from "./Input.module.css"
 
+
+
 function InputContributor() {
+  const [formData, setFormData] = useState({
+    title: '',
+    message: ''
+  })
+  const [list, setList] = useState([])
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setList([...list, formData])
+  }
+
+  const handleChange = (e) => {
+    const { name, value } = e.target
+    setFormData({ ...formData, [name]: value })
+  }
+
+
+
   return (
     <div>
-        <div class={Styles.topCont}>
-          <h2>New Post</h2>
+      <div class={Styles.topCont}>
+        <h2>New Post</h2>
+      </div>
+      <div class={Styles.titleCont}>
+        <div>
+          <h4>Title</h4>
+          <form action="">
+
+            <input type="text" onChange={handleChange} value={formData.title} name="title" id="" class={Styles.input1} />
+          </form>
         </div>
-        <div class={Styles.titleCont}>
-          <div>
-              <h4>Title</h4>
-              <input type="text" class={Styles.input1} />
-          </div>
-          <div>
-              <h4>Article</h4>
-              <h3 class={Styles.inputTop}>B   S   %   $</h3>
-              <textarea class={Styles.input2} >Start Writing</textarea>
-              <h3 class={Styles.inputBottom}>CHARS:0  WORDS:0  POWERED BY:OOU</h3>
-          </div>
-          <div>
-            <Link to="/Posts" className={Styles.Link}><button>Publish</button></Link>
-          </div>
+        <div>
+          <h4>Article</h4>
+          <h3 class={Styles.inputTop}>B   S   %   $</h3>
+          <form action="">
+            <textarea name="message" class={Styles.input2} onChange={handleChange} value={formData.message}  id="" cols="30" rows="10">Start Writing</textarea>
+          </form>
+          <h3 class={Styles.inputBottom}>CHARS:0  WORDS:0  POWERED BY:OOU</h3>
         </div>
+        <div>
+          <Link to="/Posts" className={Styles.Link}><button type='submit' onClick={handleSubmit} >Publish</button></Link>
+        </div>
+      </div>
     </div>
   )
 }
