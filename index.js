62b6196fc27b35dfb38bace0d23438cc94f6c795
shakiroh55 <fhatshakirat@gmail.com>
2023-10-10 16:35:07 +0100

    new changes made

---
+++ index.js
@@ -0,0 +1,13 @@
+const title = document.querySelector(".new-title")
+const article = document.querySelector(".new-story-content")
+const publisher =document.querySelector(".publish-btn")
+var sup = document.querySelector(".title-in")
+var body = document.querySelector(".article-in")
+// var topic = document.querySelector(".title-in").value
+// var content = document.querySelector(".article-in").value
+console.log(sup.value)
+publisher.addEventListener ("click", ()=>{
+    console.log(5+5)
+}
+)
+
